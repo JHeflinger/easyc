594f0eaecbd1b96cf4c3edfc8996f506707906d6
Jason Heflinger <jasonheflinger@gmail.com>
2026-08-25 12:26:03 -0700

    Update easyfile.c

--- include/easyfile.c
+++ include/easyfile.c
@@ -5,6 +5,53 @@
 #include <stdlib.h>
 #include <string.h>
 
+#ifndef __WIN32
+    #include <sys/types.h>
+    #include <unistd.h>
+    #include <dirent.h>
+#else
+    #define WIN32_LEAN_AND_MEAN
+    #define NOGDICAPMASKS     // CC_*, LC_*, PC_*, CP_*, TC_*, RC_
+    #define NOVIRTUALKEYCODES // VK_*
+    #define NOWINMESSAGES     // WM_*, EM_*, LB_*, CB_*
+    #define NOWINSTYLES       // WS_*, CS_*, ES_*, LBS_*, SBS_*, CBS_*
+    #define NOSYSMETRICS      // SM_*
+    #define NOMENUS           // MF_*
+    #define NOICONS           // IDI_*
+    #define NOKEYSTATES       // MK_*
+    #define NOSYSCOMMANDS     // SC_*
+    #define NORASTEROPS       // Binary and Tertiary raster ops
+    #define NOSHOWWINDOW      // SW_*
+    #define OEMRESOURCE       // OEM Resource values
+    #define NOATOM            // Atom Manager routines
+    #define NOCLIPBOARD       // Clipboard routines
+    #define NOCOLOR           // Screen colors
+    #define NOCTLMGR          // Control and Dialog routines
+    #define NODRAWTEXT        // DrawText() and DT_*
+    #define NOGDI             // All GDI defines and routines
+    #define NOKERNEL          // All KERNEL defines and routines
+    #define NOUSER            // All USER defines and routines
+    #define NOMB              // MB_* and MessageBox()
+    #define NOMEMMGR          // GMEM_*, LMEM_*, GHND, LHND, associated routines
+    #define NOMETAFILE        // typedef METAFILEPICT
+    #define NOMSG             // typedef MSG and associated routines
+    #define NOOPENFILE        // OpenFile(), OemToAnsi, AnsiToOem, and OF_*
+    #define NOSCROLL          // SB_* and scrolling routines
+    #define NOSERVICE         // All Service Controller routines, SERVICE_ equates, etc.
+    #define NOSOUND           // Sound driver routines
+    #define NOTEXTMETRIC      // typedef TEXTMETRIC and associated routines
+    #define NOWH              // SetWindowsHook and WH_*
+    #define NOWINOFFSETS      // GWL_*, GCL_*, associated routines
+    #define NOCOMM            // COMM driver routines
+    #define NOKANJI           // Kanji support stuff.
+    #define NOHELP            // Help engine interface.
+    #define NOPROFILER        // Profiler interface.
+    #define NODEFERWINDOWPOS  // DeferWindowPos routines
+    #define NOMCX             // Modem Configuration Extensions
+    #include <windows.h>
+    #include <direct.h>
+#endif
+
 static const char* FileExtension(const char* path) {
     const char* dot = strrchr(path, '.');
     const char* slash1 = strrchr(path, '/');
@@ -100,4 +147,20 @@ BOOL ez_next_line(ez_FileParser* lp, char* buffer, size_t size) {
     memcpy(buffer, lp->file->data + lp->cursor, last_ind - lp->cursor);
     lp->cursor = last_ind + 1;
     return TRUE;
+}
+
+BOOL ez_file_exists(const char* filename) {
+	#ifndef __WIN32
+		struct stat statbuf;
+        if (stat(file, &statbuf) != 0) {
+            return 0;
+        }
+        return !S_ISDIR(statbuf.st_mode);
+	#else
+		struct _stat statbuf;
+        if (_stat(file, &statbuf) != 0) {
+            return 0;
+        }
+        return (statbuf.st_mode & _S_IFDIR) == 0;
+	#endif
 }
